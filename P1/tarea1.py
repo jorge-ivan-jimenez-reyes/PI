72b720b01a579989c6d447a88e2c2560aa3becd5
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-19 13:13:18 -0600

    added PI

---
+++ P1/tarea1.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageOps
+
+def generate_negative(image_path, output_path):
+    # Open the image
+    image = Image.open(image_path)
+
+    # Generate the negative
+    negative_image = ImageOps.invert(image.convert("RGB"))
+
+    # Save the negative image
+    negative_image.save(output_path)
+    print(f"Negative image saved to {output_path}")
+
+if __name__ == "__main__":
+    # Example usage
+    input_image_path = "minion.jpg"  # Replace with your input image path
+    output_image_path = "bananoide3000.jpg"  # Replace with your desired output image path
+
+    generate_negative(input_image_path, output_image_path)
